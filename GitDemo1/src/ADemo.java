509ae3d1305bf4af93efb0cfdbff6de9eb590ded
shubhammorankar7998 <shubhammorankar7998@gmail.com>
2022-04-25 10:50:50 +0530

    Method Added

--- GitDemo1/src/ADemo.java
+++ GitDemo1/src/ADemo.java
@@ -1,9 +1,16 @@
 
 public class ADemo {
-	
+
+	public void m1() {
+		System.out.println("This is M1 method");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("A Demo Git");
+
+		ADemo a1 = new ADemo();
+		a1.m1();
 	}
 
 }
