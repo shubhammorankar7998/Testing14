859c5b904abf0550ad43bdbd73ad3ab153fee68f
shubhammorankar7998 <76561459+shubhammorankar7998@users.noreply.github.com>
2022-04-25 10:58:16 +0530

    second method

--- GitDemo1/src/ADemo.java
+++ GitDemo1/src/ADemo.java
@@ -4,6 +4,10 @@ public class ADemo {
 	public void m1() {
 		System.out.println("This is M1 method");
 	}
+	
+		public void m2() {
+		System.out.println("This is M2 method");
+	}
 
 	public static void main(String[] args) {
 
